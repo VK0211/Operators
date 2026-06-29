011b6337efd1a7330192838ae8bdefcf23dbd86b
VK0211 <vihaan.khosla91@masonohioschools.com>
2026-06-29 14:26:22 -0400

    Add files via upload

---
+++ Count_the_notes.py
@@ -0,0 +1,5 @@
+a=int(input("How much are you withdrawing? $"))
+b=a//100
+c=(a%100)//50
+d=((a%100)%50)//10
+print(b,c,d)
---
+++ Percentage.py
@@ -0,0 +1,7 @@
+a=int(input("What are your marks for math? "))
+b=int(input("What are your marks for english? "))
+c=int(input("What are your marks for science? "))
+d=int(input("What are your marks for hindi? "))
+e=a+b+c+d
+f=(e/400)*100
+print("Your marks is",f)
